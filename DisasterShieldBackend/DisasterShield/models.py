7f99e476cf7db35f4c2487a5dd8177a80fd7625b
Nithin Raj <nithinraj0404@gmail.com>
2024-07-19 17:26:41 +0530

    added missing person table

--- DisasterShieldBackend/DisasterShield/models.py
+++ DisasterShieldBackend/DisasterShield/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+import uuid
 
 # Create your models here.
+
+class missingPerson(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    date_created = models.DateTimeField(auto_now_add=True, editable=False)
+    age = models.IntegerField()
+    gender = models.CharField(max_length=1)
+    status = models.BooleanField(default=False)
+    desc = models.TextField()
+    image = models.ImageField(upload_to='static/images/', null=True)
+
