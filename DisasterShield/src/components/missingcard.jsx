d4cc9fb5f5de2ff0259b10bac40cfc08db092352
Sreehari <sreeharisanjeev2004@gmail.com>
2024-07-19 20:30:25 +0530

    Third frontend commit

--- DisasterShield/src/components/missingcard.jsx
+++ DisasterShield/src/components/missingcard.jsx
@@ -1,6 +1,11 @@
-const MissingCard = () => {
+const MissingCard = ({name,age,location,image}) => {
     return (
-        <div className="w-[80vh] min-h-20 ring-1 ring-slate-900/5 shadow-lg">
+        <div className="p-5 rounded-md ring-1 flex gap-y-4 flex-col items-start ring-slate-900/5 shadow-lg">
+            <img src={image} className="h-44 w-44 rounded-md" alt="" />
+            <h2 className="font-semibold text-xl"><span className="text-slate-700">Name: </span>{name}</h2>
+            <h2 className="font-semibold text-xl"><span className="text-slate-700">Age: </span>{age}</h2>
+            <h2 className="font-semibold text-xl"><span className="text-slate-700">Location: </span>{location}</h2>
+            <button className="py-1.5 w-full font-semibold rounded-md text-white text-lg bg-green-600">Found</button>
 
         </div>
     )
--- DisasterShield/src/pages/missing.jsx
+++ DisasterShield/src/pages/missing.jsx
@@ -1,14 +1,18 @@
 import missing from '../assets/question.png'
+import MissingCard from '../components/missingcard';
 const Missing = () => {
     return (
         <div className="p-5">
             <div className='flex gap-x-2 items-center'>
             <h1 className="font-semibold text-2xl md:text-4xl ">REPORT MISSING PEOPLE</h1>
             <img src={missing} className='h-8 w-8' alt="" />
-            <div className='flex'>
-                <div></div>
-                <div></div>
+            
             </div>
+            <div className='flex p-5'>
+                <div>
+                    <MissingCard name={'Nishant'} age={19} location={'Trivandrum'} image={'https://th.bing.com/th/id/R.b42d077f7608ecfe94a08b5d4213eb66?rik=0hx9eqp4xZnIiQ&riu=http%3a%2f%2fgetwallpapers.com%2fwallpaper%2ffull%2f0%2fb%2f5%2f175341.jpg&ehk=EG20ra9%2fkw8bRtQ5LCMLWYQ4lE4GJ7WoUAw3QT7CP3s%3d&risl=&pid=ImgRaw&r=0'} />
+                </div>
+                <div></div>
             </div>
         </div>
     )
