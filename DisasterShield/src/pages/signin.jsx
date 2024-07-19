baf43dcb7acd6a3c8de10659b6cb5d87ca6969c4
Nishanth <nishanthrn1@gmail.com>
2024-07-19 21:04:43 +0530

    merge

--- DisasterShield/src/pages/signin.jsx
+++ DisasterShield/src/pages/signin.jsx
@@ -1,70 +1,125 @@
-import React, { useContext, useState } from 'react'
-import { Link } from 'react-router-dom';
-import medical from '../assets/medical.jpg'
+import React, { useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
+import medical from "../assets/medical.jpg";
+
 function SignUp() {
-  const [username, setUserName] = useState('');
-  const [password, setPassword] = useState('');
-  const [name, setName]=useState('');
-  
-  
+  const [password, setPassword] = useState("");
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [message, setMessage] = useState("");
+
+  const navigate = useNavigate();
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    if (name === "name") {
+      setName(value);
+    } else if (name === "email") {
+      setEmail(value);
+    } else if (name === "pass") {
+      setPassword(value);
+    } else if (name === "username") {
+      setUserName(value);
+    }
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      const response = await fetch("http://192.168.126.77:8000/api/register/", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          username: name,
+          email: email,
+          password: password,
+        }),
+      });
+
+      if (!response.ok) {
+        throw new Error("Registration failed");
+      }
+
+      const data = await response.json();
+      console.log(data);
+
+      setMessage("Registration successful!");
+      setEmail("");
+      setPassword("");
+      setName("");
+      navigate("/signin");
+    } catch (error) {
+      setMessage(error.message);
+    }
+  };
+
   return (
-    <div class="flex items-center justify-center min-h-screen bg-gray-100">
-    <div
-      class="relative flex flex-col m-6 space-y-8 bg-white shadow-2xl rounded-2xl md:flex-row md:space-y-0"
-    >
-      <div class="flex flex-col justify-center p-8 md:p-14">
-        <span class="mb-3 text-4xl font-bold">Register your account</span>
-        <span class="font-light text-gray-400 mb-8">
-          Enter your details
-        </span>
-        <div class="py-4">
-          <span class="mb-2 text-md">Name</span>
-          <input
-            type="text"
-            class="w-full p-2 border border-gray-300 rounded-md placeholder:font-light placeholder:text-gray-500"
-            name="name"
-            id="name"
+    <div className="flex items-center justify-center min-h-screen bg-gray-100">
+      <div className="relative flex flex-col m-6 space-y-8 bg-white shadow-2xl rounded-2xl md:flex-row md:space-y-0">
+        <div className="flex flex-col justify-center p-8 md:p-14">
+          <span className="mb-3 text-4xl font-bold">Register your account</span>
+          <span className="font-light text-gray-400 mb-8">
+            Enter your details
+          </span>
+          {message && <div className="mb-4 text-red-500">{message}</div>}
+          <form onSubmit={handleSubmit}>
+            <div className="py-4">
+              <span className="mb-2 text-md">Name</span>
+              <input
+                onChange={handleChange}
+                value={name}
+                type="text"
+                className="w-full p-2 border border-gray-300 rounded-md placeholder:font-light placeholder:text-gray-500"
+                name="name"
+                id="name"
+              />
+            </div>
+            <div className="py-4">
+              <span className="mb-2 text-md">Email</span>
+              <input
+                onChange={handleChange}
+                value={email}
+                type="email"
+                className="w-full p-2 border border-gray-300 rounded-md placeholder:font-light placeholder:text-gray-500"
+                name="email"
+                id="email"
+              />
+            </div>
+            <div className="py-4">
+              <span className="mb-2 text-md">Password</span>
+              <input
+                onChange={handleChange}
+                value={password}
+                type="password"
+                name="pass"
+                id="pass"
+                className="w-full p-2 border border-gray-300 rounded-md placeholder:font-light placeholder:text-gray-500"
+              />
+            </div>
+            <button className="w-full bg-black text-white p-2 rounded-lg mb-6 hover:bg-white hover:text-black hover:border hover:border-gray-300">
+              Sign Up
+            </button>
+            <div className="text-center text-gray-400">
+              Already have an account?
+              <Link to="/login">
+                <span className="font-bold text-black"> Login now!</span>
+              </Link>
+            </div>
+          </form>
+        </div>
+
+        <div className="relative">
+          <img
+            src={medical}
+            alt="img"
+            className="w-[400px] h-full hidden rounded-r-2xl md:block object-cover"
           />
         </div>
-        <div class="py-4">
-          <span class="mb-2 text-md">Email</span>
-          <input
-            type="email"
-            class="w-full p-2 border border-gray-300 rounded-md placeholder:font-light placeholder:text-gray-500"
-            name="email"
-            id="email"
-          />
-        </div>
-        <div class="py-4">
-          <span class="mb-2 text-md">Password</span>
-          <input
-            type="password"
-            name="pass"
-            id="pass"
-            class="w-full p-2 border border-gray-300 rounded-md placeholder:font-light placeholder:text-gray-500"
-          />
-        </div>
-        <button
-          class="w-full bg-black text-white p-2 rounded-lg mb-6 hover:bg-white hover:text-black hover:border hover:border-gray-300"
-        >
-          Sign Up
-        </button>
-        <div class="text-center text-gray-400">
-          Already have an account?
-          <Link to="/login"><span class="font-bold text-black">Login now!</span></Link>
-        </div>
-      </div>
-    
-      <div class="relative">
-        <img
-          src={medical}
-          alt="img"
-          class="w-[400px] h-full hidden rounded-r-2xl md:block object-cover"
-        />
       </div>
     </div>
-  </div>
-  )
+  );
 }
 
-export default SignUp
+export default SignUp;
--- DisasterShieldBackend/DisasterShield/views.py
+++ DisasterShieldBackend/DisasterShield/views.py
@@ -1,10 +1,12 @@
-# views.py
+from rest_framework.permissions import AllowAny
 from rest_framework import status
 from rest_framework.response import Response
 from rest_framework.views import APIView
 from .serializers import UserRegistrationSerializer
 
 class UserRegistrationView(APIView):
+    permission_classes = [AllowAny]
+
     def post(self, request):
         serializer = UserRegistrationSerializer(data=request.data)
         if serializer.is_valid():
