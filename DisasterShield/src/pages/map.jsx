4be9df84704d1fd205921b151cd5649d767d4be7
Nishanth <nishanthrn1@gmail.com>
2024-07-19 18:42:39 +0530

    map sett

--- DisasterShield/src/pages/map.jsx
+++ DisasterShield/src/pages/map.jsx
@@ -1,43 +1,43 @@
-import { MapContainer, TileLayer, useMap, Popup, Marker } from 'react-leaflet'
-import "leaflet/dist/leaflet.css"
-import { useEffect, useState } from 'react';
-import {useGeolocated} from "react-geolocated"
-  function Map() {
-    const [longitude, setLongitude] = useState(0);
-    const [latitude, setLatitude] = useState(0);
-    const getLocation = () => {
-        const { coords, isGeolocationAvailable, isGeolocationEnabled } =
-            useGeolocated({
-                positionOptions: {
-                    enableHighAccuracy: false,
-                },
-                userDecisionTimeout: 5000,
-            });
-        if (isGeolocationAvailable && isGeolocationEnabled) {
-            setLatitude(coords.latitude);
-            setLongitude(coords.longitude);
-            console.log(coords.latitude),
-            console.log(coords.longitude)
-        }
-    };
-    
-    useEffect(()=>{
-        getLocation();
-    },[])
-    return (
-        
-            <MapContainer center={[latitude, longitude]} zoom={20} scrollWheelZoom={false}>
-  <TileLayer
-    attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-    url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-  />
-  <Marker position={[latitude, longitude]}>
-    <Popup>
-      A pretty CSS3 popup. <br /> Easily customizable.
-    </Popup>
-  </Marker>
-</MapContainer>
-        
+import { MapContainer, TileLayer, Popup, Marker } from "react-leaflet";
+import "leaflet/dist/leaflet.css";
+import { useEffect, useState } from "react";
+
+function Map() {
+  const [position, setPosition] = useState({ latitude: 0, longitude: 0 });
+
+  useEffect(() => {
+    if ("geolocation" in navigator) {
+      navigator.geolocation.getCurrentPosition(function (position) {
+        setPosition({
+          latitude: position.coords.latitude,
+          longitude: position.coords.longitude,
+        });
+      });
+    } else {
+      console.log("Geolocation is not available in your browser.");
+    }
+  }, []);
+
+  return (
+    position.latitude !== 0 &&
+    position.longitude !== 0 && (
+      <MapContainer
+        center={[position.latitude, position.longitude]}
+        zoom={10}
+        scrollWheelZoom={false}
+      >
+        <TileLayer
+          attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+        />
+        <Marker position={[position.latitude, position.longitude]}>
+          <Popup>
+            A pretty CSS3 popup. <br /> Easily customizable.
+          </Popup>
+        </Marker>
+      </MapContainer>
     )
-  }
-  export default Map;
+  );
+}
+
+export default Map;
