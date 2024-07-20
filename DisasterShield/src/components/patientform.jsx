a8304a11c5210ad788eaba385368b2b330c3e15d
Nithin Raj <nithinraj0404@gmail.com>
2024-07-20 05:51:07 +0530

    change blood donation form

--- DisasterShield/src/components/patientform.jsx
+++ DisasterShield/src/components/patientform.jsx
@@ -1,16 +1,38 @@
 import { useState } from "react";
 const PatientForm = () => {
     const [person, setPerson] = useState({
-        name: '',
+        patient_name: '',
+        priority: '',
+        gender: '',
         age: null,
-        group: '',
-        image: null,
+        blood_type: '',
+        contact_number: '',
     })
     const handleChange = (e) => {
         setPerson({...person, [e.target.name]:e.target.value});
     }
-    const handleImage = (e) => {
-        setPerson({...person, image: e.target.files[0]});
+     
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        console.log(person);
+        fetch("http://120.0.0.1:8000/api/blooddonation/",{
+            method: "POST",
+            headers: {
+                Accept: 'application/json',
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify(person)
+        })
+    }
+    const handleChecked = (e) => {
+        if(e.target.checked){
+            setPerson({...person, priority: true});
+        }else{
+            setPerson({...person, priority: false});
+        }
+    }
+    const handleRadio = (e) => {
+        setPerson({...person, gender: e.target.id});
     }
     return(
         <div className="mt-5 ml-52 p-5">
@@ -18,11 +40,11 @@ const PatientForm = () => {
             <div className="rounded-md ring-1 mt-5 ring-slate-900/5 shadow-lg p-5 flex justify-start gap-y-3 flex-col ">
             <label htmlFor="" className="font-semibold text-lg">Name</label>
             <input
-                value={person.name}
+                value={person.patient_name}
                 onChange={handleChange}
                 type="text"
                 className="w-full p-2 border border-gray-300 rounded-md placeholder:font-light placeholder:text-gray-500"
-                name="name"
+                name="patient_name"
                 id="name"
               />
               <label htmlFor="" className="font-semibold text-lg">Age</label>
@@ -37,25 +59,37 @@ const PatientForm = () => {
               />
               <label htmlFor="" className="text-lg font-semibold">Group</label>
               <input
-                value={person.group}
+                value={person.blood_type}
                 onChange={handleChange}
                 type="text"
                 className="w-full p-2 border border-gray-300 rounded-md placeholder:font-light placeholder:text-gray-500"
-                name="group"
+                name="blood_type"
                 id="name"
               />
-              <input type="file" onChange={handleImage} name="image" class="block w-full text-sm text-slate-500
-                file:mr-4 file:py-2 file:px-4
-                file:rounded-full file:border-0
-                file:text-sm file:font-semibold
-                file:bg-violet-50 file:text-violet-700
-                hover:file:bg-violet-100
-                "/>
-                {person.image != null ? <img className='h-52 w-full' src={URL.createObjectURL(person.image)} alt="" /> : null}
-                <button className='text-lg font-semibold py-1.5 px-4 bg-black text-white rounded-md'>Submit</button>
+              <label htmlFor="" className="text-lg font-semibold">Contact</label>
+              <input
+                value={person.contact_number}
+                onChange={handleChange}
+                type="text"
+                className="w-full p-2 border border-gray-300 rounded-md placeholder:font-light placeholder:text-gray-500"
+                name="contact_number"
+                id="contact"
+              />
+              <div className="flex items-center gap-x-4">
+              <label htmlFor="" className="text-lg font-semibold">Priority</label>
+              <input type="checkbox" onClick={handleChecked} />
+              </div>
+              <label htmlFor="" className="text-lg font-semibold">Gender</label>
+              <div className="flex items-center gap-x-4">
+                <span>Male</span><input onClick={handleRadio} type="radio" id="M" name="gender" />
+              <span>Female</span><input onClick={handleRadio} type="radio" id="F" name="gender" />
+              </div>
+
+              
+                <button onClick={handleSubmit} className='text-lg font-semibold py-1.5 px-4 bg-black text-white rounded-md'>Submit</button>
             </div>
 
-            
+    
         </div>
     )
 }
